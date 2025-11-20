3f0433bc610893bca141e4d8037ee2cc4d8024a2
Teerapat Chantra <basteerapat06a1@gmail.com>
2025-11-20 09:48:55 +0700

    Add basic HTML structure with PHP greeting

---
+++ exc1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    echo "Hello, World!";
+    echo "<br>";
+    echo "<h2>My Name is Bas</h2>";
+    ?>
+</body>
+</html>
